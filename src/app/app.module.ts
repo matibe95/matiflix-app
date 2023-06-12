2f14328107be5c16f2fe8419b03e510f9307cafa
matibe95 <matibel2005@gmail.com>
2023-06-12 00:53:05 -0300

    agregar modulos y nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomAsideComponent } from './custom-aside/custom-aside.component';
+import { CustomNavbarComponent } from './custom-navbar/custom-navbar.component';
+import { FeedComponent } from './feed/feed.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomAsideComponent,
+    CustomNavbarComponent,
+    FeedComponent,
+    MovieCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
