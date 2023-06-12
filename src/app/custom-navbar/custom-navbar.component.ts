911f5ca48e6b05fb38ec2602860a752c93598d5e
matibe95 <matibel2005@gmail.com>
2023-06-12 00:51:48 -0300

    make customNavbar component

---
+++ src/app/custom-navbar/custom-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-navbar',
+  templateUrl: './custom-navbar.component.html',
+  styleUrls: ['./custom-navbar.component.css']
+})
+export class CustomNavbarComponent {
+
+}
