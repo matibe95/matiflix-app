488ec6c6ea9043b0341e22b8ef0ddfa90d843b1e
matibe95 <matibel2005@gmail.com>
2023-06-12 00:49:37 -0300

    movie card component

---
+++ src/app/models/moviecard.model.ts
@@ -0,0 +1,16 @@
+export interface MovieCard {
+  // Id: number;
+  Title: string
+  Year: number
+  Rated: string
+  Genre: string
+  Director: string
+  Poster: string
+  Plot: string
+  
+  // description: string
+  // genero: string
+  // image: string
+  // director: string
+  // year: string
+}
